e8b7658945eb5862c213482f8f724a8eee48697b
Jorge-Costa0 <liocosta07@email.com>
2026-03-30 20:11:29 +0000

    fix: correcting the parameters of the run function

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import subprocess
 
-# Example: Run the 'ls -l' command (Unix/Linux/macOS) or equivalent 'dir' (Windows)
-# Use 'dir' on Windows, 'ls -l' on Unix
-try:
-    # For Unix-like systems (Linux, macOS)
-    subprocess.run(["ls", "-l"])
-except FileNotFoundError:
-    # For Windows
-    subprocess.run(["cmd", "/c", "dir"])
+
+resultado = subprocess.run(['arp', '-a'],capture_output = True,text = True)
+print(resultado.stderr)
