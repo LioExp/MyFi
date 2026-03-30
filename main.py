04e61c25dca11183661bdfba5845ac50578b4062
Jorge-Costa0 <liocosta07@email.com>
2026-03-30 21:09:12 +0000

    add: new visual

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import subprocess
 
-
-resultado = subprocess.run(['arp', '-a'],capture_output = True,text = True)
-print(resultado.stderr)
+resultado = subprocess.run(['arp', '-a'], capture_output=True, text=True)
+linhas = resultado.stdout.split()
+Nome_dispositivo = f'Nome: {linhas[0]} \n IP: {linhas[1]} \n MAC:{linhas[3]} \n Interface:{linhas[6]}'
+print(linhas)
+print('---'*9)
+print(Nome_dispositivo)
